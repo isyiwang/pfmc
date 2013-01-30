aff5b93a963aa5e94bce5b85ebaf42e291439b6e
isyiwang <isaacwang@gmail.com>
2013-01-29 21:22:10 -0800

    Renaming / restructuring files

---
+++ client/client.js
@@ -0,0 +1,16 @@
+Template.nav.items =
+  [
+    {'name': 'Services', 'link': '/services'}
+    {'name': 'Pastors', 'link': '/pastors'}
+    {'name': 'Sermons', 'link': '/sermons'}
+    {'name': 'About', 'link': '/about'}
+  ]
+
+Template.main.jesus = ->
+  choices = ["Love", "Life", "Hope", "Good", "King"]
+  choices[Math.floor((Math.random() * choices.length))];
+
+Template.hello.events
+  'click input': ->
+    if typeof console != 'undefined'
+      console.log "You pressed the button!"
